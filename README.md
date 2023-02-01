af860574b8c7ac8121fe2892289e1f060e819c70
Andy Dremeaux <adremeaux@gmail.com>
2023-02-01 16:51:48 -0500

    fix readme formatting

--- README.md
+++ README.md
@@ -4,21 +4,26 @@ Code generator for creating C# Algebraic Data Type implementations from a templa
 
 Based on Facebook's Remodel tool: https://github.com/facebook/remodel
 
-Compile for .NET 7+ (https://github.com/dotnet/roslyn):
+#### Build
+
+Compile for .NET 7+ (https://github.com/dotnet/roslyn):  
 `csc remodel.cs`
 
-Running:
-`.remodel <dir>`
+#### Run
 
-When run with ./remodel <dir>, will recursively search for all .adtValue files in the
+`./remodel <dir>`
+
+When run with `./remodel <dir>`, will recursively search for all .adtValue files in the
 directories and generate corresponding <class_name>.cs files in the same directory
 
-Format:
-`#Comment. No trailing comments.
+#### adtValue File Format
+
+```
+#Commented Line. No trailing comments allowed.
 <Set arguments>
 <classname> {
   <baseLevelVarType> <name>
-  %<baseLevelVarWithDefaultType> <name> 
+  %<baseLevelVarWithDefaultType> <name>
   #<fixedLineInsertion>
   <fieldname1> {
     <fieldVarType> <name>
@@ -26,23 +31,31 @@ Format:
   <fieldname2> {
     <fieldVarType> <name>
   }
-}`
+}
+```
 
-Modifiers:
-Preceding a base level variable with % will generate it with a default value in the constructor:
+#### Modifiers:
+
+- Preceding a base level variable with `%` will generate it with a default value in the constructor:
+
+```
+//SomeField.adtValue
 //int i
 //%SomeClass sc
+
 public sealed class SomeField {
-public SomeField(int i, SomeClass sc = default(SomeClass)) {
+  public SomeField(int i, SomeClass sc = default(SomeClass)) {
+  ...
+```
 
-Preceding a base level line with a # will cause that lined to be inserted into the base class
-as-is with no modification. You can supply numerous lines. You usually shouldn't use this as it breaks the
-standard for the pattern.
+- Preceding a base level line with a `#` will cause that lined to be inserted into the base class
+  as-is with no modification. You can supply numerous lines. You usually shouldn't use this as it breaks the
+  standard for the pattern.
 
-"Set" Arguments:
-At the start of the file, type "Set <some_arg>" to enable specific features:
+- `Set` Arguments:
+  At the start of the file, type `Set <some_arg>` to enable specific features:
 
-Set AllowNulls
-This will create null defaults in the match functions so you don't have to match the whole thing
-Set KnownTypes
-This will generate [KnownType...] directives at the top for json serialization with Newtonsoft
+  - `Set AllowNulls`
+    This will create null defaults in the match functions so you don't have to match the whole thing
+  - `Set KnownTypes`
+    This will generate `[KnownType...]` directives at the top for json serialization with Newtonsoft
--- src/remodel.cs
+++ src/remodel.cs
@@ -4,44 +4,63 @@ using System.IO;
 using System.Linq;
 
 /*
-Generate ADTs from an .adtValue file
+# CSharpRemodelADT
 
-When run with ./remodel <dir>, will recursively search for all .adtValue files in the
-directory and generate corresponding <class_name>.cs files in the same directory
+Code generator for creating C# Algebraic Data Type implementations from a template
 
-Format:
-#Comment. No trailing comments.
+Based on Facebook's Remodel tool: https://github.com/facebook/remodel
+
+#### Build
+Compile for .NET 7+ (https://github.com/dotnet/roslyn):  
+`csc remodel.cs`
+
+#### Run  
+`./remodel <dir>`
+
+When run with `./remodel <dir>`, will recursively search for all .adtValue files in the
+directories and generate corresponding <class_name>.cs files in the same directory
+
+#### adtValue File Format
+```
+#Commented Line. No trailing comments allowed.
 <Set arguments>
 <classname> {
   <baseLevelVarType> <name>
-  %<baseLevelVarWithDefaultType> <name>
+  %<baseLevelVarWithDefaultType> <name> 
   #<fixedLineInsertion>
-  ...
-  <fieldname> {
+  <fieldname1> {
     <fieldVarType> <name>
-    ...
   }
-  ...
+  <fieldname2> {
+    <fieldVarType> <name>
+  }
 }
+```
 
-Modifiers:
-Preceding a base level variable with % will generate it with a default value in the constructor:
-  //int i
-  //%SomeClass sc
-  public sealed class SomeField {
-    public SomeField(int i, SomeClass sc = default(SomeClass)) {
+#### Modifiers:
+* Preceding a base level variable with `%` will generate it with a default value in the constructor:
+```
+//SomeField.adtValue
+//int i
+//%SomeClass sc
 
-Preceding a base level line with a # will cause that lined to be inserted into the base class
+public sealed class SomeField {
+  public SomeField(int i, SomeClass sc = default(SomeClass)) {
+  ...
+```
+
+* Preceding a base level line with a `#` will cause that lined to be inserted into the base class
 as-is with no modification. You can supply numerous lines. You usually shouldn't use this as it breaks the
 standard for the pattern.
 
-"Set" Arguments:
-At the start of the file, type "Set <some_arg>" to enable specific features:
+* `Set` Arguments:
+At the start of the file, type `Set <some_arg>` to enable specific features:
 
-  Set AllowNulls
-    This will create null defaults in the match functions so you don't have to match the whole thing
-  Set KnownTypes
-    This will generate [KnownType...] directives at the top for json serialization with Newtonsoft
+  * `Set AllowNulls`
+This will create null defaults in the match functions so you don't have to match the whole thing
+  * `Set KnownTypes`
+This will generate `[KnownType...]` directives at the top for json serialization with Newtonsoft
+
 */
 
 class Remodel {
